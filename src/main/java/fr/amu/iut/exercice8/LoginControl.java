15436dd2b95553d132a4f2779a5b73cefa8902a9
Arsena <claire@A-F005606>
2024-05-22 17:07:35 +0200

    login fxml

--- src/main/java/fr/amu/iut/exercice8/LoginControl.java
+++ src/main/java/fr/amu/iut/exercice8/LoginControl.java
@@ -1,12 +1,46 @@
 package fr.amu.iut.exercice8;
 
-import javafx.scene.layout.GridPane;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-public class LoginControl extends GridPane {
+public class LoginControl {
 
+    @FXML
+    private TextField userIdField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button okButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private void initialize() {
+        // Initialize any additional setup here
+        okButton.setOnAction(event -> okClicked());
+        cancelButton.setOnAction(event -> cancelClicked());
+    }
+
+    @FXML
     private void okClicked() {
+        String userId = userIdField.getText();
+        String password = passwordField.getText();
+        int passwordLength = password.length();
+
+        System.out.println("User ID: " + userId);
+        System.out.println("Password: " + "*".repeat(passwordLength));
     }
 
+    @FXML
     private void cancelClicked() {
+        userIdField.clear();
+        passwordField.clear();
     }
+
 }
+
