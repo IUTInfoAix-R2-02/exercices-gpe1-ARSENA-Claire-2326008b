a31bbca0e7b86c3db23da213f1a5266e7ac4dd76
Arsena <claire@A-F005606>
2024-05-23 17:14:00 +0200

    getter and setters

--- src/main/java/fr/amu/iut/exercice12/CustomButton.java
+++ src/main/java/fr/amu/iut/exercice12/CustomButton.java
@@ -5,7 +5,24 @@ import javafx.scene.control.Button;
 
 public class CustomButton extends Button {
 
+    public String getCouleur() {
+        return couleur;
+    }
+
     private String couleur;
+
+    public int getNbClics() {
+        return nbClics.get();
+    }
+
+    public void setNbClics(int nbClics) {
+        this.nbClics.set(nbClics);
+    }
+
+    public IntegerProperty nbClicsProperty() {
+        return nbClics;
+    }
+
     private IntegerProperty nbClics;
 
     public CustomButton(String texte, String couleur) {
